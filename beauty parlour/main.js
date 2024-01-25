a672775ac2fdeb52e4ad9903d14c9324902c07a5
suman biswas <72162271+iconic-in@users.noreply.github.com>
2024-01-25 17:00:06 +0530

    Add files via upload

---
+++ beauty parlour/main.js
@@ -0,0 +1,16 @@
+
+
+let menuBar = document.querySelector('#menu');
+let navBar = document.querySelector('.navbar');
+
+menuBar.addEventListener('click',() => {
+
+    menuBar.classList.toggle('fa-times');
+    navBar.classList.toggle('nav-toggle');
+
+});
+
+window.onscroll = () => {
+    menuBar.classList.remove('fa-times');
+    navBar.classList.remove('nav-toggle');
+}
